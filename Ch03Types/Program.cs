0faf9bb84486ffa347bba743d66e52edbbda71ee
gabrielkf <gabrielkf@gmail.com>
2022-12-27 20:36:17 -0300

    HashCode comparison

--- Ch03Types/Program.cs
+++ Ch03Types/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Ch03Types.Structs;
 
 namespace Ch03Types
@@ -7,12 +8,36 @@ namespace Ch03Types
     {
         static void Main(string[] args)
         {
+            CompareHashCode();
             PrintText("Chapter 03. Run tests");
         }
 
         static void PrintText(string? text)
         {
             Console.WriteLine(text ?? "Text is null");
+        }
+
+        static void CompareHashCode()
+        {
+            var arr1 = new int[] { 1, 2 };
+            var arr2 = new int[] { 1, 2 };
+            // Console.WriteLine(arr1.GetHashCode());
+            // Console.WriteLine(arr2.GetHashCode());
+
+            var t1 = (1, 2);
+            var t2 = (1, 2);
+            Console.WriteLine((uint)t1.GetHashCode());
+            Console.WriteLine((uint)t2.GetHashCode());
+            
+            var dict = new Dictionary<int[], int>
+            {
+                { new int[] { 1, 2 }, 0 },
+                { new int[] { 1, 2 }, 1 }
+            };
+            foreach (var entry in dict)
+            {
+                Console.WriteLine(entry.Key.GetHashCode());
+            }
         }
     }
 }
