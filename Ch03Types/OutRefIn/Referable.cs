832cf5e78d8aff4843dc6f52277762fde8263adf
gabrielkf <gabrielkf@gmail.com>
2022-12-28 12:59:20 -0300

    Doing the same with class method also not compiles

--- Ch03Types/OutRefIn/Referable.cs
+++ Ch03Types/OutRefIn/Referable.cs
@@ -9,10 +9,18 @@ namespace Ch03Types.OutRefIn
         public ref int GetArrayElementRef(int index) => ref items[index];
         public ref int GetSameRef(ref int arg) => ref arg;
 
+        /* WILL NOT COMPILE
         public ref int GetLocalVariableRef()
         {
             int v = 420;
             return ref v;
+        }
+        */
+
+        public ref int GetLocalRefWithMethod()
+        {
+            int i = 420;
+            return ref GetSameRef(ref i);
         }
     }
 }
